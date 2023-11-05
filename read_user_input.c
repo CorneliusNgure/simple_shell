55d8363008c0c215708fe1264badf13c03dbfb0a
CorneliusNgure <kingcornelius07@gmail.com>
2023-11-05 08:30:44 -0500

    read_user_input.c

--- read_user_input.c
+++ read_user_input.c
@@ -1,6 +1,80 @@
 #include "simple_shell.h"
 
 /**
+ * _strdup - mimics the strdup function.
+ * @src: the string to be duplicated.
+ *
+ * Return: the duplicated string.
+ */
+
+char _strdup(char *src)
+{
+	char *dest = (char *)malloc(_strlen(src));
+
+	if (src == NULL)
+		return (NULL);
+
+	if (dest == NULL)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	_strncpy(dest, src);
+
+	return (dest);
+}
+
+
+
+/**
+ * _strncpy - copies a string to another.
+ *
+ * @dest: the string to be copied to.
+ * @src: the string to be copied from.
+ * @n: the exact number of bytes to be copied.
+ *
+ * Return: pointer to the copied string (dest).
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (count < n && src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	while (*s != '\0')
+	{
+		string_length++;
+		s++;
+	}
+
+	return (string_length);
+}
+
+/**
  * readUserInput - reads the input command.
  *
  * Return: the user input.
@@ -14,12 +88,12 @@ char *readUserInput()
 	if (fgets(input, sizeof(input), stdin) == NULL)
 	return (NULL);
 
-	if (strlen(input) > 0 && input[strlen(input) - 1] == '\n')
+	if (_strlen(input) > 0 && input[_strlen(input) - 1] == '\n')
 	{
-		input[strlen(input) - 1] = '\0';
+		input[_strlen(input) - 1] = '\0';
 	}
 
-	userInput = strdup(input);
+	userInput = _strdup(input);
 	if (userInput == NULL)
 	{
 		perror("strdup");
