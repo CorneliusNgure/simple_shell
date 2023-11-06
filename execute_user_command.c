c975fe0bc18180007c214b3e2fecea327e7673b1
CorneliusNgure <kingcornelius07@gmail.com>
2023-11-06 02:33:21 -0500

    execute_user_command.c

--- execute_user_command.c
+++ execute_user_command.c
@@ -1,8 +1,29 @@
 #include "simple_shell.h"
+
+/**
+ * _strcpy - Copies a string from source to destination.
+ * @dest: The destination buffer.
+ * @src: The source string to be copied.
+ * Return: A pointer to the destination buffer.
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	char *dest_start = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+
+	*dest = '\0';
+
+	return (dest_start);
+}
+
 /**
  * run_user_command - executes the user input command.
  * @command: the user input.
- * Return: void.
  */
 void run_user_command(char *command)
 {
